7ceaa0c04c68ac9c883352c1d5f410fec46fd190
Documenter.jl <documenter@juliadocs.github.io>
2023-05-06 19:17:21 +0000

    build based on 6f2171b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RSToolbox","category":"page"},{"location":"#RSToolbox","page":"Home","title":"RSToolbox","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RSToolbox.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RSToolbox]","category":"page"}]
+}
