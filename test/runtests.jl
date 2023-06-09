b1d21496c011392542b1730e4dfe0947bab727b8
JoshuaBillson <jmbillson@outlook.com>
2023-06-09 14:56:32 -0600

    Implemented Sentinel2 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,8 +46,8 @@ end
     for index in [mndwi, ndwi, ndvi, ndmi, nbri, ndbi, savi]
         result = index(sentinel_sr)
         @test result isa AbstractArray{Float32}
-        @test maximum(result) <= 1.0f0
-        @test minimum(result) >= -1.0f0
+        @test result |> skipmissing |> maximum <= 1.0f0
+        @test result |> skipmissing |> minimum >= -1.0f0
         @test visualize(result) isa AbstractArray{Gray{N0f8}}
     end
 
