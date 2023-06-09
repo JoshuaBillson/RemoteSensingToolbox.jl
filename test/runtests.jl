0d6b1c661fad5f574bca85d8731648c4731d6d08
JoshuaBillson <jmbillson@outlook.com>
2023-06-09 14:37:03 -0600

    Implemented Sentinel2 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,12 +12,43 @@ using Pipe: @pipe
         @test visualize(landsat, color) isa AbstractArray{RGB{N0f8}}
     end
 
+    # Test DN to Reflectance
+    @test @pipe landsat |> dn_to_reflectance |> skipmissing(_.stack) |> minimum |> all([x >= 0.001f0 for x in _])
+    @test @pipe landsat |> dn_to_reflectance |> skipmissing(_.stack) |> maximum |> all([x <= 0.999f0 for x in _])
+
     # Test Indices
-    for index in [mndwi, ndwi, ndvi, ndmi, nbri, ndbi]
-        result = index(landsat)
+    landsat_sr = landsat |> dn_to_reflectance
+    for index in [mndwi, ndwi, ndvi, ndmi, nbri, ndbi, savi]
+        result = index(landsat_sr)
         @test result isa AbstractArray{Float32}
         @test maximum(result) <= 1.0f0
         @test minimum(result) >= -1.0f0
         @test visualize(result) isa AbstractArray{Gray{N0f8}}
     end
+
+end
+
+@testset "Sentinel" begin
+
+    sentinel = Sentinel2A("data/sentinel/")
+
+    # Test Visualization
+    for color in [TrueColor, Agriculture, ColorInfrared, Geology, SWIR]
+        @test visualize(sentinel, color) isa AbstractArray{RGB{N0f8}}
+    end
+
+    # Test DN to Reflectance
+    @test @pipe sentinel |> dn_to_reflectance |> skipmissing(_.stack) |> minimum |> all([x >= 0.001f0 for x in _])
+    @test @pipe sentinel |> dn_to_reflectance |> skipmissing(_.stack) |> maximum |> all([x <= 0.999f0 for x in _])
+
+    # Test Indices
+    sentinel_sr = sentinel |> dn_to_reflectance
+    for index in [mndwi, ndwi, ndvi, ndmi, nbri, ndbi, savi]
+        result = index(sentinel_sr)
+        @test result isa AbstractArray{Float32}
+        @test maximum(result) <= 1.0f0
+        @test minimum(result) >= -1.0f0
+        @test visualize(result) isa AbstractArray{Gray{N0f8}}
+    end
+
 end
