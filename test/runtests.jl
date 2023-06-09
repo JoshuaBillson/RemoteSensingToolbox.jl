5676ac9161a793530ed485b9ece3238bcb8e0dfe
JoshuaBillson <jmbillson@outlook.com>
2023-06-08 22:30:49 -0600

    Added test cases for landsat

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
 using RemoteSensingToolbox
 using Test
+using Images
+using Pipe: @pipe
 
-@testset "RemoteSensingToolbox.jl" begin
-    # Write your tests here.
+@testset "Landsat" begin
+
+    landsat = Landsat8("data/landsat/"; ext="tif")
+
+    # Test Visualization
+    for color in [TrueColor, Agriculture, ColorInfrared, Geology, SWIR]
+        @test visualize(landsat, color) isa AbstractArray{RGB{N0f8}}
+    end
+
+    # Test Indices
+    for index in [mndwi, ndwi, ndvi, ndmi, nbri, ndbi]
+        result = mndwi(landsat)
+        @test result isa AbstractArray{Float32}
+        @test maximum(result) <= 1.0f0
+        @test minimum(result) >= -1.0f0
+    end
 end
